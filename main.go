f34a1519f3e8d4fdd0bcc4996992476460fdbd74
Kian Shahi <96079030+JesusKian@users.noreply.github.com>
2023-01-19 15:32:34 +0330

    🟢 main.go : Updated

--- main.go
+++ main.go
@@ -2,37 +2,29 @@ package main
 
 import (
 	"github.com/JesusKian/WriteUp/src/config"
-	// "github.com/JesusKian/WriteUp/src/sql"
 	"github.com/JesusKian/WriteUp/src/request"
-	// "github.com/JesusKian/WriteUp/src/structure"
+	_ "github.com/JesusKian/WriteUp/src/sql"
 
 	"bufio"
 	"os"
 	"sync"
-	// "encoding/xml"
-	// "fmt"
-	// "io/ioutil"
-	// "log"
-	// "net/http"
 )
-
-func init() {
-	config.ConnectToSqlDatabase()
-	config.DatabaseStatus()
-}
 
 func main() {
 	var (
-		err     error           = nil
-		file    *os.File        = &os.File{}
 		wg      *sync.WaitGroup = &sync.WaitGroup{}
 		scanner *bufio.Scanner  = &bufio.Scanner{}
+		file    *os.File        = &os.File{}
+		err     error           = nil
 	)
+
 	file, err = os.Open("./static/txt/urls.txt")
 	if err != nil {
-
+		config.SetLog("E", "main() -> Couldn't Open urls.txt File")
+		config.SetLog("D", err.Error())
 	}
 	defer file.Close()
+
 	scanner = bufio.NewScanner(file)
 	for scanner.Scan() {
 		wg.Add(1)
@@ -41,22 +33,12 @@ func main() {
 			wg.Done()
 		}(scanner.Text())
 	}
-	wg.Wait()
-	// rss := structure.Rss{}
-	// res, err := http.Get("https://medium.com/feed/tag/ctf")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer res.Body.Close()
 
-	// data, err := ioutil.ReadAll(res.Body)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// xml.Unmarshal(data, &rss)
-	// for i, j := range rss.Channel[0].Items {
-	// 	fmt.Println(i)
-	// 	fmt.Println(j.Title)
-	// 	fmt.Println()
-	// }
+	err = scanner.Err()
+	if err != nil {
+		config.SetLog("E", "main() -> Couldn't Read urls.txt Data")
+		config.SetLog("D", err.Error())
+	}
+
+	wg.Wait()
 }
